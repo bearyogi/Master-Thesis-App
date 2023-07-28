c834b2ab56f826b41ab358e133ba71b77ff50d20
Michał Mamla <mmichal1999@gmail.com>
2023-07-28 11:38:30 +0200

    init commit

---
+++ src/main/java/com/mm/masterthesis/MasterThesisApplication.java
@@ -0,0 +1,13 @@
+package com.mm.masterthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterThesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mm/masterthesis/MasterThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.masterthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
